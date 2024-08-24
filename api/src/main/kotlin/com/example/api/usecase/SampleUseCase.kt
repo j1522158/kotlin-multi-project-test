4e6e3557919b752635fb2c0560e6461656168923
j1522158 <j1522158vu@gmail.com>
2024-08-24 17:49:01 +0900

    cache clear

---
+++ api/src/main/kotlin/com/example/api/usecase/SampleUseCase.kt
@@ -0,0 +1,5 @@
+package com.example.api.usecase
+
+class SampleUseCase {
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
+rootProject.name = "kotlin-multi-project-test"
+// apiとbatchとcommonの3つのprojectを含める
+include("api", "batch", "common")
