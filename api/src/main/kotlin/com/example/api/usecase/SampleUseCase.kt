b0fdc3fb4b17f160ea3b2738f4f7f157e71df810
j1522158 <j1522158vu@gmail.com>
2024-08-24 17:48:56 +0900

    cache clear

--- api/src/main/kotlin/com/example/api/usecase/SampleUseCase.kt
+++
@@ -1,5 +0,0 @@
-package com.example.api.usecase
-
-class SampleUseCase {
-
-}
--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
-}
-
-rootProject.name = "kotlin-multi-project-test"
-// apiとbatchとcommonの3つのprojectを含める
-include("api", "batch", "common")
-include("api")
